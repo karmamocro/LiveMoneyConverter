e226ff545032dfd285b0d04bb19d2353610c5411
karmamocro <o.elhajoui@student.fontys.nl>
2018-02-07 01:49:48 +0100

    Add project files.

---
+++ LiveConverter/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace LiveConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bunifuSeparator1 = new Bunifu.Framework.UI.BunifuSeparator();
+            this.bunifuFlatButton2 = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.btnCopy = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.btnConvert = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.lblValue = new System.Windows.Forms.Label();
+            this.lblCRate = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbLogger = new System.Windows.Forms.RichTextBox();
+            this.tbValueToExchange = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.ddCurrency2 = new Bunifu.Framework.UI.BunifuDropdown();
+            this.ddCurrency1 = new Bunifu.Framework.UI.BunifuDropdown();
+            this.SuspendLayout();
+            // 
+            // bunifuSeparator1
+            // 
+            this.bunifuSeparator1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuSeparator1.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(105)))), ((int)(((byte)(105)))));
+            this.bunifuSeparator1.LineThickness = 1;
+            this.bunifuSeparator1.Location = new System.Drawing.Point(10, 525);
+            this.bunifuSeparator1.Name = "bunifuSeparator1";
+            this.bunifuSeparator1.Size = new System.Drawing.Size(639, 35);
+            this.bunifuSeparator1.TabIndex = 17;
+            this.bunifuSeparator1.Transparency = 255;
+            this.bunifuSeparator1.Vertical = false;
+            // 
+            // bunifuFlatButton2
+            // 
+            this.bunifuFlatButton2.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.bunifuFlatButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.bunifuFlatButton2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuFlatButton2.BorderRadius = 0;
+            this.bunifuFlatButton2.ButtonText = "Go to simple form";
+            this.bunifuFlatButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuFlatButton2.DisabledColor = System.Drawing.Color.Gray;
+            this.bunifuFlatButton2.Iconcolor = System.Drawing.Color.Transparent;
+            this.bunifuFlatButton2.Iconimage = ((System.Drawing.Image)(resources.GetObject("bunifuFlatButton2.Iconimage")));
+            this.bunifuFlatButton2.Iconimage_right = null;
+            this.bunifuFlatButton2.Iconimage_right_Selected = null;
+            this.bunifuFlatButton2.Iconimage_Selected = null;
+            this.bunifuFlatButton2.IconMarginLeft = 0;
+            this.bunifuFlatButton2.IconMarginRight = 0;
+            this.bunifuFlatButton2.IconRightVisible = true;
+            this.bunifuFlatButton2.IconRightZoom = 0D;
+            this.bunifuFlatButton2.IconVisible = false;
+            this.bunifuFlatButton2.IconZoom = 90D;
+            this.bunifuFlatButton2.IsTab = false;
+            this.bunifuFlatButton2.Location = new System.Drawing.Point(10, 566);
+            this.bunifuFlatButton2.Name = "bunifuFlatButton2";
+            this.bunifuFlatButton2.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.bunifuFlatButton2.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.bunifuFlatButton2.OnHoverTextColor = System.Drawing.Color.White;
+            this.bunifuFlatButton2.selected = false;
+            this.bunifuFlatButton2.Size = new System.Drawing.Size(635, 48);
+            this.bunifuFlatButton2.TabIndex = 14;
+            this.bunifuFlatButton2.Text = "Go to simple form";
+            this.bunifuFlatButton2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bunifuFlatButton2.Textcolor = System.Drawing.Color.White;
+            this.bunifuFlatButton2.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.btnCopy.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.btnCopy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCopy.BorderRadius = 0;
+            this.btnCopy.ButtonText = "Copy value";
+            this.btnCopy.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCopy.DisabledColor = System.Drawing.Color.Gray;
+            this.btnCopy.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnCopy.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnCopy.Iconimage")));
+            this.btnCopy.Iconimage_right = null;
+            this.btnCopy.Iconimage_right_Selected = null;
+            this.btnCopy.Iconimage_Selected = null;
+            this.btnCopy.IconMarginLeft = 0;
+            this.btnCopy.IconMarginRight = 0;
+            this.btnCopy.IconRightVisible = true;
+            this.btnCopy.IconRightZoom = 0D;
+            this.btnCopy.IconVisible = false;
+            this.btnCopy.IconZoom = 90D;
+            this.btnCopy.IsTab = false;
+            this.btnCopy.Location = new System.Drawing.Point(10, 448);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.btnCopy.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.btnCopy.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnCopy.selected = false;
+            this.btnCopy.Size = new System.Drawing.Size(635, 48);
+            this.btnCopy.TabIndex = 15;
+            this.btnCopy.Text = "Copy value";
+            this.btnCopy.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnCopy.Textcolor = System.Drawing.Color.White;
+            this.btnCopy.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.btnConvert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.btnConvert.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnConvert.BorderRadius = 0;
+            this.btnConvert.ButtonText = "Convert";
+            this.btnConvert.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConvert.DisabledColor = System.Drawing.Color.Gray;
+            this.btnConvert.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnConvert.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnConvert.Iconimage")));
+            this.btnConvert.Iconimage_right = null;
+            this.btnConvert.Iconimage_right_Selected = null;
+            this.btnConvert.Iconimage_Selected = null;
+            this.btnConvert.IconMarginLeft = 0;
+            this.btnConvert.IconMarginRight = 0;
+            this.btnConvert.IconRightVisible = true;
+            this.btnConvert.IconRightZoom = 0D;
+            this.btnConvert.IconVisible = false;
+            this.btnConvert.IconZoom = 90D;
+            this.btnConvert.IsTab = false;
+            this.btnConvert.Location = new System.Drawing.Point(207, 61);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.btnConvert.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.btnConvert.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnConvert.selected = false;
+            this.btnConvert.Size = new System.Drawing.Size(241, 48);
+            this.btnConvert.TabIndex = 16;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnConvert.Textcolor = System.Drawing.Color.White;
+            this.btnConvert.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.Font = new System.Drawing.Font("Millionaire DEMO", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValue.Location = new System.Drawing.Point(224, 198);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(0, 58);
+            this.lblValue.TabIndex = 10;
+            // 
+            // lblCRate
+            // 
+            this.lblCRate.AutoSize = true;
+            this.lblCRate.Font = new System.Drawing.Font("Millionaire DEMO", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCRate.Location = new System.Drawing.Point(224, 140);
+            this.lblCRate.Name = "lblCRate";
+            this.lblCRate.Size = new System.Drawing.Size(0, 58);
+            this.lblCRate.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Millionaire DEMO", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 58);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Value:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Millionaire DEMO", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 58);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "C Rate:";
+            // 
+            // rtbLogger
+            // 
+            this.rtbLogger.Location = new System.Drawing.Point(10, 264);
+            this.rtbLogger.Name = "rtbLogger";
+            this.rtbLogger.Size = new System.Drawing.Size(635, 178);
+            this.rtbLogger.TabIndex = 9;
+            this.rtbLogger.Text = "";
+            // 
+            // tbValueToExchange
+            // 
+            this.tbValueToExchange.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tbValueToExchange.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.tbValueToExchange.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.tbValueToExchange.HintForeColor = System.Drawing.Color.Empty;
+            this.tbValueToExchange.HintText = "";
+            this.tbValueToExchange.isPassword = false;
+            this.tbValueToExchange.LineFocusedColor = System.Drawing.Color.Blue;
+            this.tbValueToExchange.LineIdleColor = System.Drawing.Color.Gray;
+            this.tbValueToExchange.LineMouseHoverColor = System.Drawing.Color.Blue;
+            this.tbValueToExchange.LineThickness = 3;
+            this.tbValueToExchange.Location = new System.Drawing.Point(217, 20);
+            this.tbValueToExchange.Margin = new System.Windows.Forms.Padding(4);
+            this.tbValueToExchange.Name = "tbValueToExchange";
+            this.tbValueToExchange.Size = new System.Drawing.Size(213, 33);
+            this.tbValueToExchange.TabIndex = 8;
+            this.tbValueToExchange.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // ddCurrency2
+            // 
+            this.ddCurrency2.BackColor = System.Drawing.Color.Transparent;
+            this.ddCurrency2.BorderRadius = 3;
+            this.ddCurrency2.DisabledColor = System.Drawing.Color.Gray;
+            this.ddCurrency2.ForeColor = System.Drawing.Color.White;
+            this.ddCurrency2.Items = new string[] {
+        "EUR",
+        "CHF",
+        "USD",
+        "GBP",
+        "CAD",
+        "AUD",
+        "HKD",
+        "JPY"};
+            this.ddCurrency2.Location = new System.Drawing.Point(437, 18);
+            this.ddCurrency2.Name = "ddCurrency2";
+            this.ddCurrency2.NomalColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.ddCurrency2.onHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.ddCurrency2.selectedIndex = 2;
+            this.ddCurrency2.Size = new System.Drawing.Size(200, 35);
+            this.ddCurrency2.TabIndex = 6;
+            // 
+            // ddCurrency1
+            // 
+            this.ddCurrency1.BackColor = System.Drawing.Color.Transparent;
+            this.ddCurrency1.BorderRadius = 3;
+            this.ddCurrency1.DisabledColor = System.Drawing.Color.Gray;
+            this.ddCurrency1.ForeColor = System.Drawing.Color.White;
+            this.ddCurrency1.Items = new string[] {
+        "EUR",
+        "CHF",
+        "USD",
+        "GBP",
+        "CAD",
+        "AUD",
+        "HKD",
+        "JPY"};
+            this.ddCurrency1.Location = new System.Drawing.Point(10, 20);
+            this.ddCurrency1.Name = "ddCurrency1";
+            this.ddCurrency1.NomalColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.ddCurrency1.onHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.ddCurrency1.selectedIndex = 0;
+            this.ddCurrency1.Size = new System.Drawing.Size(200, 35);
+            this.ddCurrency1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 632);
+            this.Controls.Add(this.bunifuSeparator1);
+            this.Controls.Add(this.bunifuFlatButton2);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblValue);
+            this.Controls.Add(this.lblCRate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbLogger);
+            this.Controls.Add(this.tbValueToExchange);
+            this.Controls.Add(this.ddCurrency2);
+            this.Controls.Add(this.ddCurrency1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuSeparator bunifuSeparator1;
+        private Bunifu.Framework.UI.BunifuFlatButton bunifuFlatButton2;
+        private Bunifu.Framework.UI.BunifuFlatButton btnCopy;
+        private Bunifu.Framework.UI.BunifuFlatButton btnConvert;
+        private System.Windows.Forms.Label lblValue;
+        private System.Windows.Forms.Label lblCRate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbLogger;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox tbValueToExchange;
+        private Bunifu.Framework.UI.BunifuDropdown ddCurrency2;
+        private Bunifu.Framework.UI.BunifuDropdown ddCurrency1;
+    }
+}
+
---
+++ LiveConverter/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace LiveConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            WebClient wbClient = new WebClient();
+
+            string url = "http://api.fixer.io/latest?base=" + ddCurrency1.selectedValue + "&symbols=" + ddCurrency2.selectedValue;
+            rtbLogger.AppendText("\n" + url + "\n");
+
+            var json = wbClient.DownloadString(url);
+            rtbLogger.AppendText(json.ToString());
+
+            JObject currency = JObject.Parse(json);
+            IList<JToken> results = currency["rates"].Children().ToList();
+            IList<string> searchResults = new List<string>();
+
+            foreach (JToken result in results)
+            {
+                rtbLogger.AppendText("\n" + result.Last.ToString() + "\n");
+
+                searchResults.Add(result.Last.ToString());
+            }
+            double num1 = Convert.ToDouble(searchResults[0]);
+            double resultX = Convert.ToDouble(tbValueToExchange.Text) * num1;
+            lblCRate.Text = num1.ToString();
+            lblValue.Text = resultX.ToString();
+        }
+    }
+}
